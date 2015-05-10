433d7bd6b32353d46f4640176faad18997247548
Mohamed Taha <fcimohamedtaha@gmail.com>
2015-05-10 03:32:55 +0200

    initial commit

---
+++ tutorial/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from rest_framework import routers
+from quickstart import views
+from django.contrib import admin
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', include(admin.site.urls)),
+]
